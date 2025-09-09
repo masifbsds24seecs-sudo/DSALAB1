2825cc3979ab9438a74b826ae0c3a45b4ea5c936
masifbsds24seecs-sudo <masif.bsds24seecs@seecs.edu.pk>
2025-09-09 15:55:38 +0500

    Add Task 4 implementation (placeholder)

---
+++ task4_string_match.cpp
@@ -0,0 +1,6 @@
+#include "task4_string_match.h"
+
+// Implementation will be added here
+int naiveSearch(const std::string& text, const std::string& pattern) {
+    return -1; // temporary placeholder
+}
