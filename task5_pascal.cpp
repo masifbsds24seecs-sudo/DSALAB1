c9f1beeee51a8916da82fec8aba08fddf478113e
masifbsds24seecs-sudo <masif.bsds24seecs@seecs.edu.pk>
2025-09-09 19:03:18 +0500

    Update task5_pascal.cpp

--- task5_pascal.cpp
+++ task5_pascal.cpp
@@ -1,6 +1,27 @@
-#include "task5_pascal.h"
+#include "pascal_utils.h"
+#include <iostream>
 
-// Implementation will be added here
-std::vector<std::vector<int>> pascalTriangle(int n) {
-    return {}; // temporary placeholder
+vector<vector<int>> generatePascalTriangle(int n) {
+    vector<vector<int>> triangle;
+    
+    if (n <= 0) return triangle;
+    
+    for (int i = 0; i < n; i++) {
+        vector<int> row(i + 1, 1);
+        for (int j = 1; j < i; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+        triangle.push_back(row);
+    }
+    
+    return triangle;
+}
+
+void printPascalTriangle(const vector<vector<int>> &triangle) {
+    for (const auto &row : triangle) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
 }
