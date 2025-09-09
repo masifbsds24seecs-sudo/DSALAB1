baf700a423a67eda0239d67e933d8e3cd52e5e5e
masifbsds24seecs-sudo <masif.bsds24seecs@seecs.edu.pk>
2025-09-09 16:08:49 +0500

    Add Task 7 implementation (placeholder)

---
+++ task7_strassen.cpp
@@ -0,0 +1,8 @@
+#include "task7_strassen.h"
+
+// Implementation will be added here
+std::vector<std::vector<int>> strassenMultiply(
+    const std::vector<std::vector<int>>& A,
+    const std::vector<std::vector<int>>& B) {
+    return {}; // temporary placeholder
+}
