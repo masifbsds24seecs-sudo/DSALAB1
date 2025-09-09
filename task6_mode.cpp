5d74a1567446b9cf5675e1ac761611eae24d042f
masifbsds24seecs-sudo <masif.bsds24seecs@seecs.edu.pk>
2025-09-09 19:04:51 +0500

    Update task6_mode.cpp

--- task6_mode.cpp
+++ task6_mode.cpp
@@ -1,6 +1,27 @@
-#include "task6_mode.h"
+#include "stats_utils.h"
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
-// Implementation will be added here
-int findMode(const std::vector<int>& arr) {
-    return -1; // temporary placeholder
+vector<int> findMode(int arr[], int size) {
+    unordered_map<int, int> frequencyMap;
+    int maxFrequency = 0;
+    vector<int> modes;
+    
+    if (size == 0) return modes;
+    
+    for (int i = 0; i < size; i++) {
+        frequencyMap[arr[i]]++;
+        if (frequencyMap[arr[i]] > maxFrequency) {
+            maxFrequency = frequencyMap[arr[i]];
+        }
+    }
+    
+    for (const auto &pair : frequencyMap) {
+        if (pair.second == maxFrequency) {
+            modes.push_back(pair.first);
+        }
+    }
+    
+    return modes;
 }
